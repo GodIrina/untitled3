6ad83e20da62d51fbf39d8d6330da73f852c3deb
irina godonoage <godonoage@bk.ru>
2022-02-15 20:16:23 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 1895;
+        int mile = amount/20;
+
+        System.out.println("Мили с покупки билета:" + mile);
+
+
+
+
+
+    }
+}
